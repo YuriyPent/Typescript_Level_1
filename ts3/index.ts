48b61ece0c0fe82f3e202e78fe1cded76d4e40a1
Yuriy <Seaman_on_watch@mail.ru>
2019-05-28 20:04:52 +0300

    Optional, Default & Rest Params

--- ts3/index.ts
+++ ts3/index.ts
@@ -6,3 +6,19 @@ const sayWord = (word: string): string => {
 };
 
 sayWord('Scott');
+
+// ? for optional params
+// const sayWord = (word?: string): string => {
+//   console.log(word || 'Hello');
+//   return word || 'Hello';
+// };
+
+// sayWord();
+// Default params
+// Rest params work as expected
+const sayWords = (word = 'Hello', ...otherStuff: string[]): string => {
+  console.log(otherStuff);
+  return word;
+};
+
+sayWords('Scott', 'Wes');
